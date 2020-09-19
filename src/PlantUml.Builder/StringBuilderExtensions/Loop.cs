e045b8111e79d6678925ec522d3db78dd0d37566
Michaël Hompus <michael@hompus.nl>
2020-09-19 19:05:31 +0200

    Add `loop`

---
+++ src/PlantUml.Builder/StringBuilderExtensions/Loop.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text;
+
+namespace PlantUml.Builder
+{
+    public static partial class StringBuilderExtensions
+    {
+        /// <summary>
+        /// Renders the beginning of a loop.
+        /// </summary>
+        /// <param name="text">Optional text.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="stringBuilder"/> is <c>null</c>.</exception>
+        public static void StartLoop(this StringBuilder stringBuilder, string text = null)
+        {
+            stringBuilder.GroupStart(Constant.Loop, text);
+        }
+
+        /// <summary>
+        /// Renders the end of a loop.
+        /// </summary>
+        /// <param name="text">Optional text.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="stringBuilder"/> is <c>null</c>.</exception>
+        public static void EndLoop(this StringBuilder stringBuilder)
+        {
+            stringBuilder.GroupEnd();
+        }
+    }
+}
