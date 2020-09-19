d4ea13a78837d34cbf9520505a5111be1fb6d42a
Michaël Hompus <michael@hompus.nl>
2020-09-19 18:21:14 +0200

    Add possibilty to write text lines for multiline notes, headers, etc.

---
+++ src/PlantUml.Builder/StringBuilderExtensions/Text.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Text;
+
+namespace PlantUml.Builder
+{
+    public static partial class StringBuilderExtensions
+    {
+        /// <summary>
+        /// Renders a line of text.
+        /// </summary>
+        /// <param name="text">Line of text.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="stringBuilder"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="text"/> is <c>null</c>, empty of only white space.</exception>
+        public static void Text(this StringBuilder stringBuilder, string text)
+        {
+            if (stringBuilder is null) throw new ArgumentNullException(nameof(stringBuilder));
+
+            if (string.IsNullOrWhiteSpace(text)) throw new ArgumentException("A non-empty value should be provided", nameof(text));
+
+            stringBuilder.Append(text.Replace("\n", "\\n"));
+            stringBuilder.AppendNewLine();
+        }
+    }
+}
---
+++ tests/PlantUml.Builder.Tests/StringBuilderExtensions/TextTests.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Text;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlantUml.Builder.Tests
+{
+    [TestClass]
+    public class TextTests
+    {
+        [TestMethod]
+        public void StringBuilderExtensions_Text_Null_Should_ThrowArgumentNullException()
+        {
+            // Assign
+            var stringBuilder = (StringBuilder)null;
+
+            // Act
+            Action action = () => stringBuilder.Text("text line");
+
+            // Assert
+            action.Should().ThrowExactly<ArgumentNullException>()
+                .And.ParamName.Should().Be("stringBuilder");
+        }
+
+        [TestMethod]
+        public void StringBuilderExtensions_Text_NullText_Should_ThrowArgumentException()
+        {
+            // Assign
+            var stringBuilder = new StringBuilder();
+
+            // Act
+            Action action = () => stringBuilder.Text(null);
+
+            // Assert
+            action.Should().ThrowExactly<ArgumentException>()
+                .WithMessage("A non-empty value should be provided*")
+                .And.ParamName.Should().Be("text");
+        }
+
+        [TestMethod]
+        public void StringBuilderExtensions_Text_EmptyText_Should_ThrowArgumentException()
+        {
+            // Assign
+            var stringBuilder = new StringBuilder();
+
+            // Act
+            Action action = () => stringBuilder.Text(string.Empty);
+
+            // Assert
+            action.Should().ThrowExactly<ArgumentException>()
+                .WithMessage("A non-empty value should be provided*")
+                .And.ParamName.Should().Be("text");
+        }
+
+        [TestMethod]
+        public void StringBuilderExtensions_Text_WhitespaceText_Should_ThrowArgumentException()
+        {
+            // Assign
+            var stringBuilder = new StringBuilder();
+
+            // Act
+            Action action = () => stringBuilder.Text(" ");
+
+            // Assert
+            action.Should().ThrowExactly<ArgumentException>()
+                .WithMessage("A non-empty value should be provided*")
+                .And.ParamName.Should().Be("text");
+        }
+
+        [TestMethod]
+        public void StringBuilderExtensions_Text_WithText_Should_ContainTextLineWithText()
+        {
+            // Assign
+            var stringBuilder = new StringBuilder();
+
+            // Act
+            stringBuilder.Text("text line");
+
+            // Assert
+            stringBuilder.ToString().Should().Be("text line\n");
+        }
+
+        [TestMethod]
+        public void StringBuilderExtensions_Text_WithNewLine_Should_EscapeNewLine()
+        {
+            // Assign
+            var stringBuilder = new StringBuilder();
+
+            // Act
+            stringBuilder.Text("text line\nanother text line");
+
+            // Assert
+            stringBuilder.ToString().Should().Be("text line\\nanother text line\n");
+        }
+    }
+}
