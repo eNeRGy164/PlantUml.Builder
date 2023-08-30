86e9fe8b7d8b5f7766418bf0f4fe1ef3ee5f2d40
Michaël Hompus <michael@hompus.nl>
2023-08-30 12:36:30 +0200

    Restructure stringbuilder extension tests

--- tests/PlantUml.Builder.Tests/StringBuilderExtensions/AppendNewLineTests.cs
+++ tests/PlantUml.Builder.Tests/StringBuilderExtensions/AppendNewLineTests.cs
@@ -1,26 +1,12 @@
-namespace PlantUml.Builder.Tests;
+﻿namespace PlantUml.Builder.Tests;
 
 [TestClass]
 public class AppendNewLineTests
 {
     [TestMethod]
-    public void StringBuilderExtensions_AppendNewLine_Null_Should_ThrowArgumentNullException()
+    public void AppendNewLineReturnsANewLine()
     {
         // Arrange
-        var stringBuilder = (StringBuilder)null;
-
-        // Act
-        Action action = () => stringBuilder.AppendNewLine();
-
-        // Assert
-        action.Should().Throw<ArgumentNullException>()
-            .And.ParamName.Should().Be("stringBuilder");
-    }
-
-    [TestMethod]
-    public void StringBuilderExtensions_AppendNewLine_Should_ContainNewLine()
-    {
-        // Assign
         var stringBuilder = new StringBuilder();
 
         // Act
--- tests/PlantUml.Builder.Tests/StringBuilderExtensions/TrimEndTests.cs
+++ tests/PlantUml.Builder.Tests/StringBuilderExtensions/TrimEndTests.cs
@@ -5,25 +5,13 @@ namespace PlantUml.Builder.Tests;
 [TestClass]
 public class TrimEndTests
 {
+    [DataRow(EmptyString, DisplayName = "TrimEnd - Stays empty")]
+    [DataRow(AllWhitespace, DisplayName = "TrimEnd - All input is whitespace")]
     [TestMethod]
-    public void StringBuilderExtensions_TrimEnd_Null_Should_ThrowArgumentNullException()
+    public void TrimEndRemovesTrailingWhitespace(string input)
     {
         // Arrange
-        var stringBuilder = (StringBuilder)null;
-
-        // Act
-        Action action = () => stringBuilder.TrimEnd();
-
-        // Assert
-        action.Should().ThrowExactly<ArgumentNullException>()
-            .And.ParamName.Should().Be("stringBuilder");
-    }
-
-    [TestMethod]
-    public void StringBuilderExtensions_TrimEnd_WithWhitespace_Should_BeEmpty()
-    {
-        // Assign
-        var stringBuilder = new StringBuilder("   ");
+        var stringBuilder = new StringBuilder(input);
 
         // Act
         stringBuilder.TrimEnd();
@@ -32,42 +20,18 @@ public class TrimEndTests
         stringBuilder.ToString().Should().BeEmpty();
     }
 
+    [DataRow(AnyString, AnyString, DisplayName = "TrimEnd - Text without trailing whitespace remains unaltered")]
+    [DataRow("Text   ", "Text", DisplayName = "TrimEnd - Trims trailing whitespace")]
     [TestMethod]
-    public void StringBuilderExtensions_TrimEnd_TextWithWhitespace_Should_BeTextOnly()
+    public void TrimEndTrimsTrailingWhitespace(string input, string expected)
     {
         // Arrange
-        var stringBuilder = new StringBuilder("Text   ");
+        var stringBuilder = new StringBuilder(input);
 
         // Act
         stringBuilder.TrimEnd();
 
         // Assert
-        stringBuilder.ToString().Should().Be("Text");
-    }
-
-    [TestMethod]
-    public void StringBuilderExtensions_TrimEnd_OnlyText_Should_BeUnaltered()
-    {
-        // Assign
-        var stringBuilder = new StringBuilder("Text");
-
-        // Act
-        stringBuilder.TrimEnd();
-
-        // Assert
-        stringBuilder.ToString().Should().Be("Text");
-    }
-
-    [TestMethod]
-    public void StringBuilderExtensions_TrimEnd_Empty_Should_StayEmpty()
-    {
-        // Assign
-        var stringBuilder = new StringBuilder("");
-
-        // Act
-        stringBuilder.TrimEnd();
-
-        // Assert
-        stringBuilder.ToString().Should().BeEmpty();
+        stringBuilder.ToString().Should().Be(expected);
     }
 }
