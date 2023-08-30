7faf7069167d117b1335d520bbfaedbb591ffea0
Michaël Hompus <michael@hompus.nl>
2023-08-30 12:01:34 +0200

    Restructure namespace tests

---
+++ tests/PlantUml.Builder.Tests/ClassDiagrams/NamespaceTests.cs
@@ -0,0 +1,69 @@
+using static PlantUml.Builder.TestData;
+
+namespace PlantUml.Builder.ClassDiagrams.Tests;
+
+[TestClass]
+public class NamespaceTests
+{
+    [TestMethod("NamespaceStart - Name argument cannot be `null`")]
+    public void NamespaceNameCannotBeNull()
+    {
+        // Arrange
+        var stringBuilder = new StringBuilder();
+
+        // Act
+        Action action = () => stringBuilder.NamespaceStart(null);
+
+        // Assert
+        action.Should()
+            .ThrowExactly<ArgumentNullException>()
+            .WithParameterName("name");
+    }
+
+    [DataRow(EmptyString, DisplayName = "NamespaceStart - Name argument cannot be empty")]
+    [DataRow(AllWhitespace, DisplayName = "NamespaceStart - Name argument cannot be any whitespace character")]
+    [TestMethod]
+    public void NamespaceNameMustContainAValue(string name)
+    {
+        // Arrange
+        var stringBuilder = new StringBuilder();
+
+        // Act
+        Action action = () => stringBuilder.NamespaceStart(name);
+
+        // Assert
+        action.Should()
+            .ThrowExactly<ArgumentException>()
+            .WithParameterName("name");
+    }
+
+    [DynamicData(nameof(GetValidNotations), DynamicDataSourceType.Method, DynamicDataDisplayName = nameof(GetMethodTestDisplayName))]
+    [TestMethod]
+    public void NamespaceIsRenderedCorrectly(MethodExpectationTestData testData)
+    {
+        // Arrange
+        var stringBuilder = new StringBuilder();
+
+        var (method, parameters) = typeof(StringBuilderExtensions).GetExtensionMethodAndParameters(stringBuilder, testData.Method, testData.Parameters);
+
+        // Act
+        method.Invoke(null, parameters);
+
+        // Assert
+        stringBuilder.ToString().Should().Be($"{testData.Expected}\n");
+    }
+
+    private static IEnumerable<object[]> GetValidNotations()
+    {
+        // Define the valid notations and expected results for different overloads
+        yield return new object[] { new MethodExpectationTestData("NamespaceStart", "namespace name {", "name") };
+        yield return new object[] { new MethodExpectationTestData("NamespaceStart", "namespace \"Display Name\" as name {", "name", "Display Name") };
+        yield return new object[] { new MethodExpectationTestData("NamespaceStart", "namespace name <<stereotype>> {", "name", null, "stereotype") };
+        yield return new object[] { new MethodExpectationTestData("NamespaceStart", "namespace name #Blue {", "name", null, null, (Color)NamedColor.Blue) };
+        yield return new object[] { new MethodExpectationTestData("NamespaceStart", "namespace \"Display Name\" as name <<stereotype>> #Blue {", "name", "Display Name", "stereotype", (Color)NamedColor.Blue) };
+
+        yield return new object[] { new MethodExpectationTestData("NamespaceEnd", "}") };
+    }
+
+    public static string GetMethodTestDisplayName(MethodInfo _, object[] data) => TestHelpers.GetValidNotationTestDisplayName(data);
+}
